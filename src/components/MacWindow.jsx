6c213ac858037584505dd511c41bef1784f9438b
Divjot Virdi <virdidivjot@gmail.com>
2026-04-30 16:58:31 -0700

    fixed styling of pic box

--- src/components/MacWindow.jsx
+++ src/components/MacWindow.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 export default function MacWindow() {
   return (
-    <div className='bg-gray-200 flex flex-col p-4 rounded-lg shadow-lg h-100 w-full max-w-4xl mt-10'>
-     <div className='bg-blue-200 h-10 flex items-left p-2 rounded-t-lg'>
+    <div className="bg-white flex flex-col rounded-xl shadow-2xl border border-gray-200 h-150 w-full max-w-4xl mt-10 overflow-hidden transform -translate-y-2">
+        <div className="bg-white flex gap-2 items-center p-3 border-b border-gray-200">
+            <div className="bg-red-400 h-3 w-3 rounded-full"></div>
+            <div className="bg-yellow-400 h-3 w-3 rounded-full"></div>
+            <div className="bg-green-400 h-3 w-3 rounded-full"></div>
+        </div>
 
-     </div>
-     <div className='bg-green-200 h-full p-2 rounded-b-lg'>
-        Pictures will come here 
-     </div>
+        <div className="bg-green-100 h-full p-2 relative overflow-hidden">
+            Pictures will come here
+        </div>
     </div>
   );
 }
